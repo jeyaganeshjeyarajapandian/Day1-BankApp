4583a86eb5be61b9887b23e15b681607a365433e
jeyaganeshjeyarajapandian <jeyaganesh.jeyarajapandian@capgemini.com>
2017-11-24 15:51:03 +0530

    Test1

--- BankApp/Day1-BankApp/src/main/java/org/cap/dao/AccountDao.java
+++ BankApp/Day1-BankApp/src/main/java/org/cap/dao/AccountDao.java
@@ -3,12 +3,7 @@ package org.cap.dao;
 import org.cap.dto.Account;
 import org.cap.dto.Customer;
 
-public interface AccountDao {
-	
-	public boolean createAccount(Account account);
-	
+public interface AccountDao {	
+	public boolean createAccount(Account account);	
 	public Account findAccountById(int accountNo);
-	
-	
-
 }
